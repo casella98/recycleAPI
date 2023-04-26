c014dd953630b55d272f46e6d7395375cd8baabd
Francesco Casella <francescocasella@MacBook-Air-di-Francesco.local>
2023-04-26 15:28:02 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+
+const mondayResponses = ["Carta", "Plastica"];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const day = new Date().getDay();
+  switch (day) {
+    case 0 || 2 || 4:
+      res.send("Umido");
+      break;
+    case 1:
+      const weekNumber = getWeekNumber(new Date());
+      const responseIndex = weekNumber % 2; // Operatore modulo per alternare le risposte
+
+      res.send(mondayResponses[responseIndex]);
+      break;
+    case 3:
+      res.send("Plastica");
+      break;
+    case 5:
+      res.send("Indifferenziato");
+      break;
+  }
+});
+
+// Funzione per ottenere il numero della settimana dell'anno
+function getWeekNumber(date) {
+  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
+  return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+}
+
+module.exports = router;
